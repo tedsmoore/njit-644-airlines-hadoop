cd340a0748fcaf9fb6fa7db7fc7a5ddfb6f517f5
tedsmoore <tedsmoore@gmail.com>
2020-08-02 12:15:00 -0400

    tweak shell scripts

--- incremental_yrs.sh
+++ incremental_yrs.sh
@@ -2,11 +2,13 @@
 
 for i in $(seq 1987 2007);
 do
-  match="((1987"
+  match="{x"
   for ((j=1987; j<$i; j++));
   do
-    match=$match")|("$j
+    match=$match","$j.csv.bz2
   done
-  match=$match"))*"
-  time bash mr_sorted.sh $1 input/$match /out_$j
+  match=$match"}"
+  j="$(($j-1))"
+  echo "Running through year: $j"
+  { time bash timing.sh $1 input/$match /out_$j ; } 2>> timing_$1.txt
 done
--- mr_sorted.sh
+++ mr_sorted.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yarn jar njit-644-airlines.jar $1 /$2/$3 /unsort_temp
-yarn jar njit-644-airlines.jar SortDescending /unsort_temp /$4
+yarn jar njit-644-airlines.jar $1 /$2 /unsort_temp
+yarn jar njit-644-airlines.jar SortDescending /unsort_temp /$3
 hdfs dfs -rm -r /unsort_temp
